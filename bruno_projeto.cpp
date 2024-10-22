0e9c7ba6d4af82a7d6fa6356c4d6a1b4e9fb63f7
Brunodev176 <brunoribeironery@gmail.com>
2024-10-22 11:45:21 -0300

    opção de saída
    incluso no sistema a opção de saída

--- bruno_projeto.cpp
+++ bruno_projeto.cpp
@@ -132,6 +132,7 @@ int main()
 		printf("\t1 - Registra nomes\n");
 		printf("\t2 - consultar nomes\n");
 		printf("\t3 - deletar nomes\n\n");
+		printf("\t4 - Sair do sistema\n\n");
 		printf("Opção:"); //fim do menu 
 	
 		scanf("%d", &opcao); //armazenamento do usuário
@@ -150,6 +151,11 @@ int main()
 			
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("obrigado por utilizar o sistema!\n");
+			return 0;
 			break;
 			
 			
